db45eb81300b08b4e3dd88d200595262103be3d2
Sarah Sexton <sarahjeannesexton@live.com>
2016-06-02 13:45:59 -0500

    updates

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 var restify = require('restify');
 var builder = require('botbuilder');
+var server = restify.createServer();
 
 var appId = process.env.MY_APP_ID || "Missing your app ID";
 var appSecret = process.env.MY_APP_SECRET || "Missing your app secret";
@@ -12,21 +13,22 @@ bot.add('/', function (session) {
 session.send('Hello World');
 });
 
-// Setup Restify Server
-var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3978, function () {
-console.log('%s listening to %s', server.name, server.url);
-});
-
 //This is for an index.html home page
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-//var port = process.env.PORT || 3000;
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
-app.use(express.static(__dirname));
+var port = process.env.PORT || 3000;
+ app.get('/', function(req, res){
+   res.sendFile(__dirname + '/index.html');
+ });
+ 
+// Setup Restify Server
+//var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3000, function () {
+console.log('%s listening to %s', server.name, server.url);
+});
+
+//app.use(express.static(__dirname));
 //redirecting, telling it to use the thing that's in the directory name 
 //(should default to index.html)
