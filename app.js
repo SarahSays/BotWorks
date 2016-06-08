6b54527be6036dec7455ddeb651a7ff9cb558c46
Sarah Sexton <sarahjeannesexton@live.com>
2016-06-08 13:45:46 -0500

    Created package.json and app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-
+//Add your requirements
 var restify = require('restify');
 var builder = require('botbuilder');
 
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({appId: process.env.MY_APP_ID, appSecret: process.env.MY_APP_SECRET});
+var bot = new builder.BotConnectorBot
+({appId: process.env.MY_APP_ID, appSecret: process.env.MY_APP_SECRET});
 bot.add('/', new builder.SimpleDialog(function (session) {
     session.send('Hello World');
 }));
