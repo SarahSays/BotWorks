ae9b86709aa6baacb801d8afef80cf66a2bcd08b
Sarah Sexton <sarahjeannesexton@live.com>
2016-06-09 17:45:27 -0500

    iFrame front end

--- app.js
+++ app.js
@@ -2,21 +2,24 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
+var appId = process.env.MY_APP_ID || "Missing your app ID";
+var appSecret = process.env.MY_APP_SECRET || "Missing your app secret";
+
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot
 ({appId: process.env.MY_APP_ID, appSecret: process.env.MY_APP_SECRET});
-bot.add('/', new builder.SimpleDialog(function (session) {
-    session.send('Hello World');
+bot.add('/', new builder.SimpleDialog( function (session) {
+session.send('Hello World');
 }));
- 
+
 // Setup Restify Server
 var server = restify.createServer();
- 
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3000, function () {
+console.log('%s listening to %s', server.name, server.url);
+});
+
 server.get('/', restify.serveStatic({
     directory: __dirname,
     default: '/index.html'
 }));
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3000, function () {
-    console.log('%s listening to %s', server.name, server.url);
-});
